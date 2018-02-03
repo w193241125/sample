187599caf4d24179e6175c5798d1ac5db787b505
larwas <164722305@qq.com>
2018-02-03 08:14:14 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
+     * 可以批量赋值的属性
      *
      * @var array
      */
@@ -19,10 +20,17 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
+     * 对用户密码或其它敏感信息在`用户实例`通过数组或 JSON 显示时进行隐藏
      *
      * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
